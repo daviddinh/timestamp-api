089f782d5df492cedc392f1c1680ac718ef7dd33
David Dinh <daviddinh93@gmail.com>
2016-12-07 22:21:09 +0000

    finished project, need to upload to heroku

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-var express = require('express')
-var app = express()
+'use strict';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var express = require('express');
+var moment = require('moment');
+var app = express();
+
+app.get('/:time', function(req, res) {
+	var time = req.params.time;
+	var response = {
+		unix: null,
+		natural: null
+	}
+	if (moment(time).isValid()) {
+		response = {
+			unix: moment(time).unix(),
+			natural: moment(time).format('LL')
+		}
+	}
+	else {
+		if (moment.unix(time).isValid()) {
+			response = {
+				unix: moment.unix(time).unix(),
+				natural: moment.unix(time).format('LL')
+			}
+		}
+	}
+	res.send(JSON.stringify(response));
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(8080, function() {
+	console.log('Timestamp API listening on port 8080!')
 })
